fe14f0e4ca01157b457359a9d7919a59cfc226dd
andhau <andrehauberg@mac.com>
2019-07-17 20:24:12 +0200

    changed add

--- wordcount/test_git.py
+++ wordcount/test_git.py
@@ -1,9 +1,9 @@
 '''This is just a test of git'''
 
-def add(a, b):
-    return a + b
+def add(a, b, c):
+    return a + b + c
 
 def subtract(a, b):
     return a - b
 
-print(f"{add(2,3)}")
+print(f"{add(2,3,4)}")
